09d65eff03762eb27c5573f8d4297058b9b4db63
ShehacksPurple <shehacksPurple@github.com>
2018-10-11 17:44:56 -0400

    Added some unit tests

---
+++ UnitTests.DevSlop/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevSlop.Models;
+
+namespace UnitTests.DevSlop
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mock = new ErrorViewModel();
+            Assert.AreEqual(mock.ShowRequestId, false);
+
+            mock.RequestId = "Hi Morgan"; 
+            Assert.AreEqual(mock.ShowRequestId, true);
+
+            mock.RequestId = "";
+            Assert.AreEqual(mock.ShowRequestId, false);
+
+        }
+    }
+}
