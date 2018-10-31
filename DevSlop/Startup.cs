d4ae0a1bf90c3a75110e312960af0dc46a383edb
ShehacksPurple <shehacksPurple@github.com>
2018-10-30 21:32:18 -0400

    “securityheaders”

--- DevSlop/Startup.cs
+++ DevSlop/Startup.cs
@@ -89,6 +89,9 @@ namespace DevSlop
                 app.UseHsts();
             }
 
+            //Security headers make me happy
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());             app.UseXContentTypeOptions();             app.UseReferrerPolicy(opts => opts.NoReferrer());             app.UseXXssProtection(options => options.EnabledWithBlockMode());             app.UseXfo(options => options.Deny());              app.UseCsp(opts => opts                 .BlockAllMixedContent()                 .StyleSources(s => s.Self())                 .StyleSources(s => s.UnsafeInline())                 .FontSources(s => s.Self())                 .FormActions(s => s.Self())                 .FrameAncestors(s => s.Self())                 .ImageSources(s => s.Self())                 .ScriptSources(s => s.Self())             );             //End Security Headers
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
