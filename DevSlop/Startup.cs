208603297ed382684a07f147a7fdee9e43c329a2
ShehacksPurple <shehacksPurple@github.com>
2018-12-30 14:31:52 -0500

    upgraded .Net core, bootstrap, jquery and replaced security headers

--- DevSlop/Startup.cs
+++ DevSlop/Startup.cs
@@ -92,8 +92,27 @@ namespace DevSlop
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+                app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
             }
+
+            //Security headers make me happy
+           app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.Deny());
+            app.UseCsp(opts => opts
+             .BlockAllMixedContent()
+             .StyleSources(s => s.Self())
+             .StyleSources(s => s.UnsafeInline())
+             .FontSources(s => s.Self())
+             .FormActions(s => s.Self())
+             .FrameAncestors(s => s.Self())
+             .ImageSources(s => s.Self())
+             .ScriptSources(s => s.Self())
+             );
+            //End Security Headers
+
+
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
